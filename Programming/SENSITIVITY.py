88550fc6cc0587d4b3eb1042396029bded162a22
MathxStudio <allan_pan_mathx123@163.com>
2022-01-19 13:07:18 +0800

    Pevious Contests added as references

--- Programming/SENSITIVITY.py
+++ Programming/SENSITIVITY.py
@@ -7,24 +7,4 @@ for i in range(0, 15) :
         s = (aD[i] - aD[j]) / aD[i]
         t = (s[i] - s[j]) / s[i]
 
-# random quiz generator
-import random
-
-# The quiz data. Keys are states and values are their capitals.
-capitals = { 'Alabama' : 'Montgomery', 'Alaska' : 'Juneau', 'Arizona' : 'Phoenix',
-             'Arkansas' : 'Little Rock', 'California' : 'Sacramento', 'Colorado' : 'Denver',
-             'Connecticut' : 'Hartford', 'Delaware' : 'Dover', 'Florida' : 'Tallahassee',
-             'Georgia' : 'Atlanta', 'Hawaii' : 'Honolulu', 'Idaho' : 'Boise', 'Illinois' :
-                 'Springfield', 'Indiana' : 'Indianapolis', 'Iowa' : 'Des Moines', 'Kansas' :
-                 'Topeka', 'Kentucky' : 'Frankfort', 'Louisiana' : 'Baton Rouge', 'Maine' :
-                 'Augusta', 'Maryland' : 'Annapolis', 'Massachusetts' : 'Boston', 'Michigan' :
-                 'Lansing', 'Minnesota' : 'Saint Paul', 'Mississippi' : 'Jackson', 'Missouri' :
-                 'Jefferson City', 'Montana' : 'Helena', 'Nebraska' : 'Lincoln', 'Nevada' :
-                 'Carson City', 'New Hampshire' : 'Concord', 'New Jersey' : 'Trenton', 'NewMexico' : 'Santa Fe',
-             'New York' : 'Albany', 'North Carolina' : 'Raleigh',
-             'North Dakota' : 'Bismarck', 'Ohio' : 'Columbus', 'Oklahoma' : 'Oklahoma City',
-             'Oregon' : 'Salem', 'Pennsylvania' : 'Harrisburg', 'Rhode Island' : 'Providence',
-             'South Carolina' : 'Columbia', 'South Dakota' : 'Pierre', 'Tennessee' :
-                 'Nashville', 'Texas' : 'Austin', 'Utah' : 'Salt Lake City', 'Vermont' :
-                 'Montpelier', 'Virginia' : 'Richmond', 'Washington' : 'Olympia', 'West Virginia' : 'Charleston',
-             'Wisconsin' : 'Madison', 'Wyoming' : 'Cheyenne' }
+# random
